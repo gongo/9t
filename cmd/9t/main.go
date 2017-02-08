a12fe242934fad49f2cab5d18558d47e69487b36
Wataru MIYAGUNI <gonngo@gmail.com>
2017-02-08 12:33:55 +0900

    Create `NineTail`

--- cmd/9t/main.go
+++ cmd/9t/main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
+	"log"
 	"os"
-	"sync"
 
 	"github.com/gongo/9t"
 )
 
 func main() {
-	var wg sync.WaitGroup
 	filenames := os.Args[1:]
 
-	for _, t := range ninetail.NewTailers(filenames) {
-		wg.Add(1)
-		go func(t *ninetail.Tailer) {
-			t.Do()
-			wg.Done()
-		}(t)
+	runner, err := ninetail.Runner(filenames, ninetail.Config{Colorize: true}) // TODO use flags!!
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	wg.Wait()
+	runner.Run()
 }
